75a81962266751f73e1dd90eae22c0158ba9c434
Bart Heerkens <bartheerkens@Barts-MacBook-Pro.local>
2021-01-11 20:17:40 +0100

    Changed hello.c to ask for a name

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<cs50.h>
 
 int main()
 {
-    printf("Hello CS50 Study Group!\n");
+    string name = get_string("Name: ");
+    printf("Hello %s!\n", name);
     return 0;
 }
